0e3229b1851ea4585c30f3432a1a193d64501ba3
岩田玲将 <iwatareo@re0.local>
2017-02-19 00:40:19 +0900

    マイページの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	def show
-		@nickname = currnet_user.nickname
+		@nickname = current_user.nickname
 		@tweets = current_user.tweets.page(params[:page]).order('created_at DESC')
+	end
 end
